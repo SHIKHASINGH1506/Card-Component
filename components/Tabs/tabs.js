1735b68815cc07171d38a539150a16ea74c020b2
Shikha <shikhakumarisingh9715@gmail.com>
2022-01-27 09:30:34 +0530

    three variants of basic cards completed

--- components/Tabs/tabs.js
+++ components/Tabs/tabs.js
@@ -1,9 +1,27 @@
 tabs = document.querySelectorAll(".tab");
+tabsTopIcon = document.querySelectorAll(".tab__icon");
+tabsLeadIcon = document.querySelectorAll(".tab__icon-lead");
 
 tabs.forEach( tab => {
     tab.addEventListener('click', () => {
         activeTabs = document.querySelector(".tab-active");
         activeTabs.classList.remove("tab-active");
         tab.classList.add("tab-active");
+    })
+})
+
+tabsTopIcon.forEach( tab => {
+    tab.addEventListener('click', () => {
+        activeTabs = document.querySelector(".tab__icon-active");
+        activeTabs.classList.remove("tab__icon-active");
+        tab.classList.add("tab__icon-active");
+    })
+})
+
+tabsLeadIcon.forEach( tab => {
+    tab.addEventListener('click', () => {
+        activeTabs = document.querySelector(".tab__icon-lead-active");
+        activeTabs.classList.remove("tab__icon-lead-active");
+        tab.classList.add("tab__icon-lead-active");
     })
 })
